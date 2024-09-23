7d4489b138c44a38610b0d91122162c9c9817b81
Amna1993 <amnarazzaqawan@gmail.com>
2024-09-23 15:41:34 -0400

    text

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 import resume from '../assets/pdf/Muhammad Waqar.pdf'
 import waqarr from '../assets/png/waqarr.png'
 export const headerData = {
-    name: 'Amna Razzaq',
+    name: 'Amna',
     title: "Front End Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: waqarr,
