d2680908de3b6dad175f33047a16f968f7b9e35b
Amna1993 <amnarazzaqawan@gmail.com>
2024-09-23 14:19:07 -0400

    Initial project commit

---
+++ src/data/projectsData.js
@@ -0,0 +1,99 @@
+import one from "../assets/svg/projects/one.svg";
+import two from "../assets/svg/projects/two.svg";
+import three from "../assets/svg/projects/three.svg";
+import four from "../assets/svg/projects/four.svg";
+import five from "../assets/svg/projects/four.svg";
+
+
+export const projectsData = [
+  {
+    id: 1,
+    projectName: "Event Management",
+    projectDesc:
+      "This project aims to build an Event management system that consolidates all information of a event booking",
+    tags: ["React", "CSS", "Material Ui"],
+    // code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    // demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    image: one,
+  },
+  {
+    id: 2,
+    projectName: "DPF(Digital Paper Flow)",
+    projectDesc:
+      "Your dashboard quickly shows you recent activity and a summary of orders and invoices. slide. Orders Screen. Visually see orders when drafting and approving",
+    tags: ["React", "Redux", "TypeScript", "CSS"],
+    // code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    // demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    image: two,
+  },
+  {
+    id: 3,
+    projectName: "Bill Bio",
+    projectDesc:
+      "Do everything with one platform. Shorten URLs create bio link pages, custom QR codes, vcard links, file links & more.",
+    tags: ["React", "JavaScript", "CSS", "Material Ui"],
+    // code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    // demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    image: three,
+  },
+   {
+        id: 4,
+        projectName: 'Comply Core',
+        projectDesc: 'The application will have different types of user accounts. The app will need to integrate with Chat GPT 3.5 Turbo and Whisper AI to enable the AI chatbots to provide efficient and accurate guidance to operators',
+        tags: ['MERN Stack development'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: four
+    },
+    {
+        id: 5,
+        projectName: 'E-Commerce App',
+        projectDesc: 'A Simple E-commerce application',
+        tags: ['React jS', 'Firebase'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: five
+    },
+    /* {
+        id: 6,
+        projectName: 'Uber Lite',
+        projectDesc: 'Uber clone',
+        tags: ['Flutter'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: six
+    },
+    {
+        id: 7,
+        projectName: 'Stock Market App',
+        projectDesc: 'A simple stock market API app',
+        tags: ['React', 'Redux', 'Bootstrap'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: seven
+    },
+    {
+        id: 8,
+        projectName: 'Car Pooling System',
+        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+        tags: ['Flutter', 'React'],
+        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        image: eight
+    }, */
+];
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
