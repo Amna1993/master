b9fe112c21c2397613ca44668f920dcae02985de
Amna1993 <amnarazzaqawan@gmail.com>
2024-09-23 17:14:54 -0400

    updated aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "Hafiza Amna Razzaq. I'm a React developer based in Toronto, Canada.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at BlockChain learning and AI. I love to learn and explore new arenas.",
+    description1: "I’m Hafiza Amna Razzaq, a highly skilled Front-End Web Developer based in Toronto, Canada, with 4+ years of experience in building dynamic, responsive web applications. I specialize in using React.js, Vue.js, and Angular frameworks to create efficient, scalable solutions that enhance user experience and optimize performance.",
+    description2: "In my professional journey, I have worked on a wide range of projects, from optimizing real-time data analytics dashboards to leading Agile development teams in crafting intuitive and high-performing web applications. I thrive in both team settings and independent projects, continuously improving my skills. With experience in software testing and a passion for development, I focus on delivering results that positively impact business outcomes.",
     image: 2
 }
